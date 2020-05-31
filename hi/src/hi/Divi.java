662d3bd9ecb7b2588ba46ba015c0c2a3cd7fd270
Akshatprajapati <60837732+Akshatprajapati@users.noreply.github.com>
2020-05-31 11:30:32 +0530

    ok

---
+++ hi/src/hi/Divi.java
@@ -0,0 +1,18 @@
+package hi;
+
+public class Divi {
+
+	public static void main(String[] args) {
+
+		int i = 2000;
+		while(i<=3000){
+		if(i%7==0 && i%5!=0){
+			
+			System.out.println(i);
+		}
+		i++;
+		}
+		
+	}
+
+}
